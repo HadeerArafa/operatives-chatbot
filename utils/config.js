8e6bf843493b314beee8f1708bede9da529744f8
saleh1312 <profsoft1312@gmail.com>
2024-08-15 18:33:19 +0300

    a

--- utils/config.js
+++ utils/config.js
@@ -1,2 +1,2 @@
 export const url = "http://localhost:3010"
-export const request_url = "http://178.16.143.6:7010" // http://178.16.143.6:7010 http://localhost:5010
+export const request_url = "http://localhost:5010" // http://178.16.143.6:7010 http://localhost:5010
