24938b2701ac9ca128b8c4159ff45026f9aaa5e8
saleh1312 <profsoft1312@gmail.com>
2024-08-15 18:02:08 +0300

    added2

--- components/chat/msgs_types/list.jsx
+++ components/chat/msgs_types/list.jsx
@@ -7,7 +7,7 @@ import HideSourceIcon from '@mui/icons-material/HideSource';
 import { useState } from 'react'
 
 
-function ListMsg({ ele, i,len }) {
+function ListMsg({ ele, i, len }) {
     const [show, set_show] = useState(false)
 
     const create_table = (ele) => {
@@ -18,7 +18,7 @@ function ListMsg({ ele, i,len }) {
                 <thead>
                     <tr className={styles.tr}>
                         {
-                            Object.keys(ele?.title[0]).map((key,index)=>(
+                            Object.keys(ele?.title[0]).map((key, index) => (
                                 <th key={index} className={styles.th}>
                                     {key}
                                 </th>
@@ -49,7 +49,7 @@ function ListMsg({ ele, i,len }) {
     if (ele.sent_by_customer === true) {
         return (
             <div className="msg_container_user" key={i} >
-                 <div className={`msg ${i+1==len?"message-pop":""}`} >
+                <div className={`msg ${i + 1 == len ? "message-pop" : ""}`} >
                     {
                         create_table(ele)
                     }
@@ -58,44 +58,44 @@ function ListMsg({ ele, i,len }) {
                 </div>
             </div>
         )
-    } else  {
+    } else {
 
-      
+
         return (
             <div className="msg_container" key={i} >
                 {/* <img src="./images/logo.png"></img> */}
                 <img src="./images/logo.jpg"></img>
-                <div className={`${i+1==len?"message-pop":""}`} style={{ display: "flex", flexDirection: "column", width: "90%" }}>
-                
-                <div style={{display:"flex", alignItems:"center"}}>
-                <div className={`msg ${ele.title=='...'?"typing-animation":""}`} >
-                <p style={{fontWeight:"bold" , paddingTop:"10px"}}>
-                    {ele.short_sentence}
-                </p>
-                        {ele.title=='...'?
-                                <span  className={`${ele.title=='...' && i+1==len?"dots":""}`} dir='rtl' style={{ wordWrap: "break-word" }}>{ele.title}</span>
+                <div className={`${i + 1 == len ? "message-pop" : ""}`} style={{ display: "flex", flexDirection: "column", width: "90%" }}>
+
+                    <div style={{ display: "flex", alignItems: "center" }}>
+                        <div className={`msg ${ele.title == '...' ? "typing-animation" : ""}`} >
+                            <p style={{ fontWeight: "bold", paddingTop: "10px" }}>
+                                {ele.short_sentence}
+                            </p>
+                            {ele.title == '...' ?
+                                <span className={`${ele.title == '...' && i + 1 == len ? "dots" : ""}`} dir='rtl' style={{ wordWrap: "break-word" }}>{ele.title}</span>
                                 :
                                 create_table(ele)
+                            }
+                            {/* <span style={{ wordWrap: "break-word" }}>{ele.title}</span> */}
+                        </div>
+                        {
+                            !show ?
+                                <button className='btn query_btn' onClick={() => { set_show(true) }}>
+                                    <VisibilityIcon style={{ marginLeft: "20px", cursor: "pointer" }} />
+                                </button>
+                                :
+                                <button className='btn query_btn' onClick={() => { set_show(false) }}>
+                                    <HideSourceIcon style={{ marginLeft: "20px", cursor: "pointer" }} />
+                                </button>
                         }
-                        {/* <span style={{ wordWrap: "break-word" }}>{ele.title}</span> */}
-                    </div>
-                    {
-                        !show?
-                    <button className='btn query_btn' onClick={()=>{set_show(true)}}>
-                    <VisibilityIcon style={{marginLeft:"20px" , cursor:"pointer"}}/>
-                         </button>
-                         :
-                         <button className='btn query_btn' onClick={()=>{set_show(false)}}>
-                        <HideSourceIcon style={{marginLeft:"20px" , cursor:"pointer"}}/>
-                         </button>
-                }
 
                     </div>
                     {
-                        show && ele.sql_query != null&&
-                        <div style={{marginTop:"25px"}} className={`msg ${ele.title=='...'?"typing-animation":""}`} >
-                        <p>{ele.sql_query}</p>
-                        
+                        show && ele.sql_query != null &&
+                        <div style={{ marginTop: "25px" }} className={`msg ${ele.title == '...' ? "typing-animation" : ""}`} >
+                            <p>{ele.sql_query}</p>
+
                         </div>
                     }
                     <p style={{ marginLeft: "15px" }}>{new Date(ele.message_date).getHours()}:{new Date(ele.message_date).getMinutes()} {new Date(ele.message_date).getHours() > 12 ? "Pm" : "Am"}</p>
@@ -103,9 +103,10 @@ function ListMsg({ ele, i,len }) {
                 </div>
             </div>
 
-            
-        )}
-    
+
+        )
+    }
+
 }
 
 export default ListMsg
